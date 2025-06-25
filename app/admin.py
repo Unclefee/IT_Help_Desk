c7448f83daf6f783547d5a34e6e07a556d73841d
Unclefee <koranteng2003@gmail.com>
2025-06-25 16:28:38 +0000

    fixed issues with the 2nd line in the admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company, Department, CompanyDepartment, User, Employee, Admin, DepartmentAdmin, Ticket, Comment, NotificationLog
+from .model import Company, Department, CompanyDepartment, User, Employee, Admin, DepartmentAdmin, Ticket, Comment, NotificationLog
 
 
 admin.site.register(Company)
