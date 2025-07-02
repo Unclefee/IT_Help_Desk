b5816312ea37fff9d242592f7b0223fad72817d1
Unclefee <109461926+Unclefee@users.noreply.github.com>
2025-07-02 15:46:30 +0000

    Routed and Registered all the models

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+from rest_framework.routers import DefaultRouter
+from .views import ( 
+CompanyViewSet, DepartmentViewSet, CompanyDepartmentViewSet,
+UserViewSet, TicketViewSet, CommentViewSet, NotificationLogViewSet
+)
 
+router = DefaultRouter()
+router.register(r'companies', CompanyViewSet)
+router.register(r'departments', DepartmentViewSet)
+router.register(r'company-departments', CompanyDepartmentViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'tickets', TicketViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'notifications', NotificationLogViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
